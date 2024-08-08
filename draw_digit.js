0b1ce240ee27d6484f3bbca581f2e453bd6ce42a
Kyra Cho <wsc2119@columbia.edu>
2024-08-07 17:44:58 -0700

    Add files via upload

---
+++ draw_digit.js
@@ -0,0 +1,36 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.lineWidth = 15;
+
+let drawing = false;
+
+canvas.addEventListener('mousedown', () => { drawing = true; });
+canvas.addEventListener('mouseup', () => { drawing = false; ctx.beginPath(); });
+canvas.addEventListener('mousemove', draw);
+
+function draw(event) {
+    if (!drawing) return;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+    ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function predictDigit() {
+    let imageData = canvas.toDataURL('image/png');
+    fetch('/predict', {
+        method: 'POST',
+        body: JSON.stringify({ image: imageData }),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('result').innerText = `Predicted Digit: ${data.prediction}`;
+    });
+}
