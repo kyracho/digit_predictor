8c6e2d3cf2a66f216503d1d06845c21042b530c8
Kyra Cho <wsc2119@columbia.edu>
2024-08-09 14:31:50 -0700

    Update digit_predictor.js

--- digit_predictor.js
+++ digit_predictor.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 let isDrawing = false;
 
 async function loadModel() {
-    model = await tf.loadLayersModel('model/model.json');
+    model = await tf.loadLayersModel('model.json');
 }
 
 function clearCanvas() {
