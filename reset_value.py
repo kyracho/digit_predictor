d25aea83849e1edc2301a17ef2530bf66054c5ca
kyracho <wsc2119@columbia.edu>
2024-08-18 18:29:06 -0700

    update reset_value.py for heroku scheduler

--- reset_value.py
+++ reset_value.py
@@ -1,8 +1,8 @@
 import os
 import psycopg2
 
-# Connect to the Heroku PostgreSQL database using the DATABASE_URL environment variable
-DATABASE_URL = os.getenv('postgres://u991akeevc4a0h:pcf0b36db9cae231cd6db2f31dab20b0f04c901657fd7b8df06b18f5098fa417b@cbec45869p4jbu.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d8i8or84v733sh')
+# Connect to database
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://u991akeevc4a0h:pcf0b36db9cae231cd6db2f31dab20b0f04c901657fd7b8df06b18f5098fa417b@cbec45869p4jbu.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d8i8or84v733sh')
 
 try:
     # Establish the connection
