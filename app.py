85067c6e3aa21ae35d6f4c266c381e4971069d1a
kyracho <wsc2119@columbia.edu>
2024-08-18 19:14:47 -0700

    reword today's to global

--- app.py
+++ app.py
@@ -49,10 +49,10 @@ def submit_number():
         if current_score > max_score:
             cursor.execute("UPDATE score_table SET value = %s WHERE id = 1;", (current_score,))
             conn.commit()
-            message = f"Today's high score:  {current_score}"
+            message = f"Global high score:  {current_score}"
             print(f"Database updated with new score: {current_score}")
         else:
-            message = f"Today's high score: {max_score}"
+            message = f"Global high score: {max_score}"
             print(f"Received score ({current_score}) is not higher than current max score({max_score})")
 
         cursor.close()
